70376244acdf3fc6f6e8b9566344b173d6ad9cde
Nnaemeka Odiegwu <benjaminodiegwu2@gmail.com>
2023-03-17 11:06:52 +0100

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
-printf("Size of a char: %ld\n bytes(s)", sizeof(char));
-printf("Size of an int: %ld\n bytes(s)", sizeof(int));
-printf("Size of a long int: %ld\n bytes(s)", sizeof(long));
-printf("Size of a long long: %ld\n bytes(s)", sizeof(long long));
-printf("Size of a float: %ld\n bytes(s)", sizeof(float));
-printf("Size of a double: %ld\n bytes(s)", sizeof(double));
-printf("Size of a long double: %ld\n bytes(s)", sizeof(long double));
+printf("Size of a char: %ld bytes(s)\n", sizeof(char));
+printf("Size of an int: %ld bytes(s)\n", sizeof(int));
+printf("Size of a long int: %ld bytes(s)\n", sizeof(long));
+printf("Size of a long long: %ld bytes(s)\n", sizeof(long long));
+printf("Size of a float: %ld bytes(s)\n", sizeof(float));
+printf("Size of a double: %ld bytes(s)\n", sizeof(double));
+printf("Size of a long double: %ld bytes(s)\n", sizeof(long double));
 return (0);
 }
