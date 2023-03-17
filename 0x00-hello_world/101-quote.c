494ea4fa3003fbe661d7d1132220a4b0bade30f7
Nnaemeka Odiegwu <benjaminodiegwu2@gmail.com>
2023-03-17 21:20:28 +0100

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,12 @@
 #include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(STDERR_FILENO, message, sizeof(message) - 1);
 return (1);
 }
