678f55b717f7a67c19adc8651cb02dce9fd0c903
Nnaemeka Odiegwu <benjaminodiegwu2@gmail.com>
2023-03-17 10:50:07 +0100

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of a char: %ld", sizeof(char));
+printf("Size of an int: %ld", sizeof(int));
+printf("Size of a long int: %ld", sizeof(long));
+printf("Size of a long long: %ld", sizeof(long long));
+printf("Size of a float: %ld", sizeof(float));
+printf("Size of a double: %ld", sizeof(double));
+printf("Size of a long double: %ld", sizeof(long double));
+return (0);
+}
