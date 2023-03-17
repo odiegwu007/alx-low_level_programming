cba6cfe2dc347bfe497605f86c958784a2d37b1f
Nnaemeka Odiegwu <benjaminodiegwu2@gmail.com>
2023-03-17 11:27:05 +0100

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-printf("Size of a char: %ld bytes(s)\n",sizeof(char));
-printf("Size of an int: %ld bytes(s)\n",sizeof(int));
-printf("Size of a long int: %ld bytes(s)\n",sizeof(long));
-printf("Size of a long long int: %ld bytes(s)\n",sizeof(long long));
-printf("Size of a float: %ld bytes(s)\n",sizeof(float));
+printf("Size of a char: %ld\n byte(s)", sizeof(char));
+printf("Size of an int: %ld\n byte(s)", sizeof(int));
+printf("Size of a long int: %ld\n byte(s)", sizeof(long int));
+printf("Size of a long long int: %ld\n byte(s)", sizeof(long long));
+printf("Size of a float: %ld\n byte(s)", sizeof(float));
 return (0);
 }
