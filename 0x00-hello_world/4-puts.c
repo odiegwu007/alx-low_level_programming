8f815cf2d6889f19e2dbd190e1a0662c46f1de12
Nnaemeka Odiegwu <benjaminodiegwu2@gmail.com>
2023-03-17 10:05:36 +0100

    Programming like building a puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,6 @@
  */
 int main(void)
 {
-char word[] = "Programming is like building a multilingual puzzle\n";
-puts(word);
+puts("Programming is like building a multilingual puzzle\n");
 return (0);
 }
