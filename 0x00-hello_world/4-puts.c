b97935df06fce7d36ce27fd99c1fdd3917b5eead
Nnaemeka Odiegwu <benjaminodiegwu2@gmail.com>
2023-03-17 10:02:46 +0100

    Programming is like a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-char word[] = "Programming is like building a multilingual puzzle";
+char word[] = "Programming is like building a multilingual puzzle\n";
 puts(word);
 return (0);
 }
