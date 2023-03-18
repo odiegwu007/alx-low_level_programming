1c80bf7f3cb86b5b21cccd386680904ba992868d
Nnaemeka Odiegwu <benjaminodiegwu2@gmail.com>
2023-03-18 04:09:52 +0100

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,18 +6,14 @@
  */
 int main(void)
 {
-int i, j;
+int i;
 for (i = 0; i < 10; i++)
 {
-for (j = i; j < 10; j++)
-{
 putchar(i + '0');
-putchar(j + '0');
-if (i != 9 || j != 9)
+if (i < 9)
 {
 putchar(',');
 putchar(' ');
-}
 }
 }
 putchar('\n');
