8ee9ec8c20bc3fa863161c4bed80587a0b62ba93
Nnaemeka Odiegwu <benjaminodiegwu2@gmail.com>
2023-03-20 20:57:06 +0100

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+int j;
+for (i = 0; i < 10; i++)
+{
+for (j = i; j < 10; j++)
+{
+if (i == j)
+{
+continue;
+}
+putchar(i + '0');
+putchar(j + '0');
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
