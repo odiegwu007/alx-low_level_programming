703515468fdeea7d8c4e0a3e4172c80ca8eb9c08
Nnaemeka Odiegwu <benjaminodiegwu2@gmail.com>
2023-03-20 17:41:50 +0100

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-fprintf(stderr, "%s\n", message);
+char m[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+fprintf(stderr, "%s\n", m);
 return (1);
 }
