26c07a0c8d4e2c304714bf94b6ad2ce993487bc8
Nnaemeka Odiegwu <benjaminodiegwu2@gmail.com>
2023-03-22 11:49:33 +0100

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: 'program prints the alphabets followed by a newline'
+ * Return: should return 0 if successful
+ */
+void print_alphabet(void)
+{
+char ch;
+for (ch='a'; ch<='z'; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+}
