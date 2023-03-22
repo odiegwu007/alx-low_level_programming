5de484512433e9ee2aa5366828f6c23a3d425b1c
Nnaemeka Odiegwu <benjaminodiegwu2@gmail.com>
2023-03-23 00:58:16 +0100

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include <ctype.h>
+#include "main.h"
+/**
+ * _islower - prints 1 if character is lowercase or uppercase
+ * @c: character to be checked
+ * Return: should return 1 if lowercase
+ */
+int _isalpha(int c)
+{
+if (isalpha(c))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+return (0);
+}
