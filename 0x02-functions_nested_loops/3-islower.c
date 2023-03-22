904688553e5bcef1c63da659f47c06c7f6cdd5b8
Nnaemeka Odiegwu <benjaminodiegwu2@gmail.com>
2023-03-22 14:17:19 +0100

    3-is_lower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - prints 1 if character is lowercase
+ * Return: should return 1 if lowercase
+ */
+int _islower(int c)
+{
+if (islower(c))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+return (0);
+}
