2034babd2612afcc69516b3da589a81f0f7ff83e
Nnaemeka Odiegwu <benjaminodiegwu2@gmail.com>
2023-03-22 09:47:39 +0100

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,7 +7,7 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c)
+int main(void)
 {
 _putchar("_");
 _putchar("p");
@@ -18,4 +18,5 @@ _putchar("h");
 _putchar("a");
 _putchar("r");
 _putchar("\n");
+return(0);
 }
