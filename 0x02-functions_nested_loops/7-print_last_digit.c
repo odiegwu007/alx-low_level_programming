4b4a0cdfafd2be1b9868c8f882bb856cbc23ed3b
Nnaemeka Odiegwu <benjaminodiegwu2@gmail.com>
2023-03-23 02:25:52 +0100

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a number in its absolute state
+ * @c: character to be checked
+ * Return: should return the result of num
+ */
+
+int print_last_digit(int c)
+{
+int num;
+num = abs(c) % 10;
+return (num);
+}
