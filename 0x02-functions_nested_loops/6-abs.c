87f18cb153c51380d60557970be525f68e7c9d20
Nnaemeka Odiegwu <benjaminodiegwu2@gmail.com>
2023-03-23 02:16:05 +0100

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * _abs - prints absolute number
+ * @c: character to be checked
+ * Return: should return the result of num
+ */
+int _abs(int c)
+{
+int num;
+num = abs(c);
+return num;
+return (0);
+}
