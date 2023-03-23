37735c1d9ba4a06b73969ccad3887a548ab874c6
Nnaemeka Odiegwu <benjaminodiegwu2@gmail.com>
2023-03-23 01:40:09 +0100

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include <ctype.h>
+#include "main.h"
+/**
+ * print_sign - prints symbols
+ * @c: character to be checked
+ * Return: should return 1, -1 and 0
+ */
+int print_sign(int c)
+{
+if (n > 0)
+{
+putchar(plus);
+return (1);
+}
+else if (n < 0)
+{
+putchar(neg);
+return (-1);
+}
+else
+{
+putchar(zero)
+return (0)
+}
+return (0);
+}
