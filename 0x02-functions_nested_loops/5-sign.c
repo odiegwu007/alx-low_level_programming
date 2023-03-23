6f869bfaa96ee24082d1b6b1eed5a606952847c9
Nnaemeka Odiegwu <benjaminodiegwu2@gmail.com>
2023-03-23 01:45:04 +0100

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,18 +12,18 @@ char neg = '-';
 char zero = '0';
 if (n > 0)
 {
-putchar(plus);
+_putchar(plus);
 return (1);
 }
 else if (n < 0)
 {
-putchar(neg);
+_putchar(neg);
 return (-1);
 }
 else
 {
-putchar(zero)
-return (0)
+_putchar(zero);
+return (0);
 }
 return (0);
 }
