1c657a4487fb41475221e20e0cbfb1e60199443a
Nnaemeka Odiegwu <benjaminodiegwu2@gmail.com>
2023-03-26 15:13:36 +0100

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: 'program prints _putchar'
+ * Return: should return 0 if successful
+ */
+void times_table(void)
+{
+int y;
+int a;
+int b;
+for (int i = 0; i < 10; i++)
+{
+for (int j = 0; j < 10; j++)
+{
+y = i * j;
+if (y < 10)
+{
+a = y;
+_putchar(a + '0');
+}
+else
+{
+a = y / 10;
+_putchar((y / 10) + '0');
+_putchar((y % 10) + '0');
+}
+if (j < 9)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
+}
